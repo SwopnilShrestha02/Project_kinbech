4f3cf03697ee9b90165356645939a2daaccbd319
SwopnilShrestha02 <Swopnilrajshrestha@gmail.com>
2023-06-18 22:43:05 +0545

    creating actions

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,18 @@
+import {PRODUCT_List_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} from '../constants/productConstants'
+import axios from 'axios'
+
+export const listProducts =()=>async(dispatch)=>{
+    try{
+        dispatch({type: PRODUCT_List_REQUEST})
+        const {data}=await axios.get('/api/products')
+        dispatch({
+            type: PRODUCT_LIST_SUCCESS,
+            payload: data
+        })
+    } catch(error){
+        dispatch({
+            type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+        })
+    }
+}
