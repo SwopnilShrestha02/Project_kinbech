a602bce6d44f6a385317d400611fac3c821d3ed0
SwopnilShrestha02 <Swopnilrajshrestha@gmail.com>
2023-06-20 09:19:10 +0545

    product screen using redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,26 +1,32 @@
-import React,{useState, useEffect} from 'react'
+import React,{ useEffect} from 'react'
 import {Link,useParams} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
 import {Row, Col ,Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import { listProductDetails } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
-
-const ProductScreen = ({match}) => {
-    const [product, setProduct]=useState({})
+const ProductScreen = () => {
     const {id}=useParams();
+    const dispatch= useDispatch()
+    const productDetails=useSelector(state=>state.productDetails)
 
-    useEffect(()=>{
-        const fetchProduct=async () =>{
-            const {data} =await axios.get(`/api/products/${id}`)
-            setProduct(data)
-        }
-        fetchProduct()
-    },[id])
+    
+    const {loading,error,product}=productDetails
+
+
+    useEffect(() => {
+          dispatch(listProductDetails(id))
+    }, [dispatch,id])
   return (
     <>
     <Link className='btn btn-light my-3' to='/'>
         Go Back
     </Link>
+    {loading?<Loader/>:error?
+    <Message variant='danger'>{error}</Message>:(
+
     <Row>
         <Col md={6}>
             <Image src={product.image} alt={product.name} fluid/>
@@ -69,6 +75,7 @@ const ProductScreen = ({match}) => {
             </Card>
         </Col>
     </Row>
+    )}
     </>
         
   )
